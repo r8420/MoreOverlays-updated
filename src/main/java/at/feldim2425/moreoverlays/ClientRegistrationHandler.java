9d09b4fb2a364b71ed47cd0ac774d2e5d1583f05
Richard Gonlag <rdgonlag@hotmail.com>
2020-11-02 01:20:18 +0100

    Fixed crash when loaded on server

--- src/main/java/at/feldim2425/moreoverlays/ClientRegistrationHandler.java
+++ src/main/java/at/feldim2425/moreoverlays/ClientRegistrationHandler.java
@@ -2,11 +2,15 @@ package at.feldim2425.moreoverlays;
 
 import at.feldim2425.moreoverlays.api.itemsearch.SlotHandler;
 import at.feldim2425.moreoverlays.chunkbounds.ChunkBoundsHandler;
+import at.feldim2425.moreoverlays.config.Config;
+import at.feldim2425.moreoverlays.gui.ConfigScreen;
 import at.feldim2425.moreoverlays.itemsearch.GuiHandler;
 import at.feldim2425.moreoverlays.itemsearch.GuiUtils;
 import at.feldim2425.moreoverlays.itemsearch.integration.MantleModuleScreenOverride;
 import at.feldim2425.moreoverlays.lightoverlay.LightOverlayHandler;
 import at.feldim2425.moreoverlays.lightoverlay.integration.AlternateLightHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraftforge.fml.ModList;
 
 public final class ClientRegistrationHandler {
@@ -35,5 +39,8 @@ public final class ClientRegistrationHandler {
         if (enable_jei && ModList.get().isLoaded("mantle")) {
             SlotHandler.INSTANCE.addPositionOverride(new MantleModuleScreenOverride());
         }
+    }
+    public static Screen openSettings(Minecraft mc, Screen modlist) {
+        return new ConfigScreen(modlist, Config.config_client, MoreOverlays.MOD_ID);
     }
 }
--- src/main/java/at/feldim2425/moreoverlays/MoreOverlays.java
+++ src/main/java/at/feldim2425/moreoverlays/MoreOverlays.java
@@ -1,9 +1,9 @@
 package at.feldim2425.moreoverlays;
 
 import at.feldim2425.moreoverlays.config.Config;
-import at.feldim2425.moreoverlays.gui.ConfigScreen;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.Screen;
+//import at.feldim2425.moreoverlays.gui.ConfigScreen;
+//import net.minecraft.client.Minecraft;
+//import net.minecraft.client.gui.screen.Screen;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -11,6 +11,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -32,16 +34,15 @@ public class MoreOverlays {
         modBus.addListener(this::onClientInit);
 
         Config.initialize();
-        ctx.registerConfig(ModConfig.Type.CLIENT, Config.config_client, MOD_ID + ".toml");
 
-        ctx.registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> this::openSettings);
+        ctx.registerConfig(ModConfig.Type.CLIENT, Config.config_client, MOD_ID + ".toml");
+        ctx.registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ctx.registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> ClientRegistrationHandler::openSettings);
     }
 
     public void onClientInit(FMLClientSetupEvent event) {
         ClientRegistrationHandler.setupClient();
     }
 
-    public Screen openSettings(Minecraft mc, Screen modlist) {
-        return new ConfigScreen(modlist, Config.config_client, MOD_ID);
-    }
+
 }
