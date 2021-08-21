0f7ae8295ffe02f8b508d11a445d40ad6fc76a3a
Richard Gonlag <rdgonlag@gmail.com>
2021-08-21 15:06:29 +0200

    Completely removed JEI intergration (TEMPORARY)

--- src/main/java/at/ridgo8/moreoverlays/ClientRegistrationHandler.java
+++ src/main/java/at/ridgo8/moreoverlays/ClientRegistrationHandler.java
@@ -1,12 +1,12 @@
 package at.ridgo8.moreoverlays;
 
-import at.ridgo8.moreoverlays.api.itemsearch.SlotHandler;
+//import at.ridgo8.moreoverlays.api.itemsearch.SlotHandler;
 import at.ridgo8.moreoverlays.chunkbounds.ChunkBoundsHandler;
 import at.ridgo8.moreoverlays.config.Config;
 import at.ridgo8.moreoverlays.gui.ConfigScreen;
-import at.ridgo8.moreoverlays.itemsearch.GuiHandler;
-import at.ridgo8.moreoverlays.itemsearch.GuiUtils;
-import at.ridgo8.moreoverlays.itemsearch.integration.MantleModuleScreenOverride;
+//import at.ridgo8.moreoverlays.itemsearch.GuiHandler;
+//import at.ridgo8.moreoverlays.itemsearch.GuiUtils;
+//import at.ridgo8.moreoverlays.itemsearch.integration.MantleModuleScreenOverride;
 import at.ridgo8.moreoverlays.lightoverlay.LightOverlayHandler;
 import at.ridgo8.moreoverlays.lightoverlay.integration.AlternateLightHandler;
 import net.minecraft.client.Minecraft;
@@ -26,19 +26,19 @@ public final class ClientRegistrationHandler {
     }
 
     public static void setupClient() {
-        enable_jei = ModList.get().isLoaded("jei");
+//        enable_jei = ModList.get().isLoaded("jei");
         KeyBindings.init();
 
         LightOverlayHandler.init();
         ChunkBoundsHandler.init();
-        GuiUtils.initUtil();
+//        GuiUtils.initUtil();
         AlternateLightHandler.init();
 
-        GuiHandler.init();
+//        GuiHandler.init();
 
-        if (enable_jei && ModList.get().isLoaded("mantle")) {
-            SlotHandler.INSTANCE.addPositionOverride(new MantleModuleScreenOverride());
-        }
+//        if (enable_jei && ModList.get().isLoaded("mantle")) {
+//            SlotHandler.INSTANCE.addPositionOverride(new MantleModuleScreenOverride());
+//        }
     }
     public static Screen openSettings(Minecraft mc, Screen modlist) {
         return new ConfigScreen(modlist, Config.config_client, MoreOverlays.MOD_ID);
