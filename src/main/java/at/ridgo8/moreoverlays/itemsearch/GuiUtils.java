6c476521e74312b8dd51e89eb67f4afbd66a6017
Richard Gonlag <rdgonlag@gmail.com>
2021-09-18 00:39:26 +0200

    Fix JEI overlay position

--- src/main/java/at/ridgo8/moreoverlays/itemsearch/GuiUtils.java
+++ src/main/java/at/ridgo8/moreoverlays/itemsearch/GuiUtils.java
@@ -13,10 +13,10 @@ public class GuiUtils {
 
     public static void initUtil() {
         try {
-            fieldLeft = ObfuscationReflectionHelper.findField(AbstractContainerScreen.class, "field_147003_i");
+            fieldLeft = ObfuscationReflectionHelper.findField(AbstractContainerScreen.class, "leftPos");
             fieldLeft.setAccessible(true);
 
-            fieldTop = ObfuscationReflectionHelper.findField(AbstractContainerScreen.class, "field_147009_r");
+            fieldTop = ObfuscationReflectionHelper.findField(AbstractContainerScreen.class, "topPos");
             fieldTop.setAccessible(true);
         } catch (ObfuscationReflectionHelper.UnableToFindFieldException e) {
             MoreOverlays.logger.error("Tried to load gui coordinate fields for reflection");
