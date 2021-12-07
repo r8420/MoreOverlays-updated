5182779d212c01895bfcc460060eb7fb49ec518b
Richard Gonlag <rdgonlag@gmail.com>
2021-12-07 23:10:55 +0100

    Added config warning back

--- src/main/java/at/ridgo8/moreoverlays/gui/ConfigScreen.java
+++ src/main/java/at/ridgo8/moreoverlays/gui/ConfigScreen.java
@@ -33,8 +33,8 @@ public class ConfigScreen extends Screen {
     private String txtDone = "";
 
     public ConfigScreen(Screen modListScreen, ForgeConfigSpec spec, String modId) {
-        super(new TranslatableComponent("gui.config." + modId + ".tile"));
-//        super(new TranslatableComponent("Config screen broken in this version - Edit config/moreoverlays.toml Manually!").withStyle(ChatFormatting.DARK_RED));
+        //super(new TranslatableComponent("gui.config." + modId + ".tile"));
+        super(new TranslatableComponent("Config screen is broken - To edit input fields please edit config/moreoverlays.toml Manually!").withStyle(ChatFormatting.DARK_RED));
         this.modListScreen = modListScreen;
         this.configSpec = spec;
         this.modId = modId;
