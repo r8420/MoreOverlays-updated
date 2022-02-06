f24cdf19c65809ffe2a7beca65c1c12302c6b8bf
Richard Gonlag <rdgonlag@gmail.com>
2022-02-06 20:39:59 +0100

    Upped version and forge

--- src/main/java/at/ridgo8/moreoverlays/mixin/MixinDebugRenderer.java
+++ src/main/java/at/ridgo8/moreoverlays/mixin/MixinDebugRenderer.java
@@ -18,12 +18,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(DebugRenderer.class)
 public class MixinDebugRenderer {
     @Inject(method = "render", at = @At("HEAD"))
-    private void render(PoseStack poseStack, MultiBufferSource.BufferSource buffer, double a, double b, double c, CallbackInfo callback) {
+    private void render(PoseStack p_113458_, MultiBufferSource.BufferSource p_113459_, double p_113460_, double p_113461_, double p_113462_, CallbackInfo ci) {
         if (ChunkBoundsHandler.getMode() != ChunkBoundsHandler.RenderMode.NONE && Minecraft.getInstance().options.graphicsMode == GraphicsStatus.FABULOUS) {
-            ChunkBoundsRenderer.renderOverlays(poseStack);
+            ChunkBoundsRenderer.renderOverlays(p_113458_);
         }
         if(LightOverlayHandler.isEnabled() && Minecraft.getInstance().options.graphicsMode == GraphicsStatus.FABULOUS){
-            LightOverlayHandler.renderer.renderOverlays(LightOverlayHandler.scanner, poseStack);
+            LightOverlayHandler.renderer.renderOverlays(LightOverlayHandler.scanner, p_113458_);
         }
     }
 }
