9b7d37466888fdbfe93630c737ba1bde54e60d2a
Richard Gonlag <rdgonlag@gmail.com>
2022-08-06 20:19:16 +0200

    Removed Mantle integration

--- src/main/java/at/ridgo8/moreoverlays/ClientRegistrationHandler.java
+++ src/main/java/at/ridgo8/moreoverlays/ClientRegistrationHandler.java
@@ -6,7 +6,6 @@ import at.ridgo8.moreoverlays.config.Config;
 import at.ridgo8.moreoverlays.gui.ConfigScreen;
 import at.ridgo8.moreoverlays.itemsearch.GuiHandler;
 import at.ridgo8.moreoverlays.itemsearch.GuiUtils;
-import at.ridgo8.moreoverlays.itemsearch.integration.MantleModuleScreenOverride;
 import at.ridgo8.moreoverlays.lightoverlay.LightOverlayHandler;
 import at.ridgo8.moreoverlays.lightoverlay.integration.AlternateLightHandler;
 import net.minecraft.client.Minecraft;
@@ -36,9 +35,6 @@ public final class ClientRegistrationHandler {
 
         GuiHandler.init();
 
-        if (enable_jei && ModList.get().isLoaded("mantle")) {
-            SlotHandler.INSTANCE.addPositionOverride(new MantleModuleScreenOverride());
-        }
         // Quick fix for light level in 1.18 (need better fix)
         if(!Config.light_FinishedMigration.get()){
             Config.light_SaveLevel.set(1);
